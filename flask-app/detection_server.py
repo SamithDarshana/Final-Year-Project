cf49dc870fedfab054d417571509097f7ad3edd5
Nimesh1010 <69566587+Nimesh1010@users.noreply.github.com>
2025-11-20 19:53:11 +0530

    gunicorn added to req.txt

--- flask-app/detection_server.py
+++ flask-app/detection_server.py
@@ -180,8 +180,8 @@ def get_mode():
         "thresholds": current_mode
     })
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))   # Render sets $PORT, fallback to 5000 locally
-    app.run(host="0.0.0.0", port=port, debug=True)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))   # Render sets $PORT, fallback to 5000 locally
+#     app.run(host="0.0.0.0", port=port, debug=True)
 # if __name__ == "__main__":
 #     app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
